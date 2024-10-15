e768c6d0c45c7a60ecec80125e8c2bdc8fd8c3b4
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-10-15 12:03:48 +0200

    Update postbuild.js

--- postbuild.js
+++ postbuild.js
@@ -1,7 +1,9 @@
 import * as fs from "fs"
 const banned=[".git"]
+const old=fs.readdirSync("./dist")
 fs.readdirSync("./dist").map((item)=>{
   console.log(item)
   if(banned.includes(item)){return}
+  fs.writeFileSync("./dist/"+item,fs.readFileSync("./dist/"+item).toString().replace(item,item.split(".")[0].split("-")[0]+"."+item.split(".")[1])
   fs.renameSync("./dist/"+item,"./dist/"+item.split(".")[0].split("-")[0]+"."+item.split(".")[1])
 })
