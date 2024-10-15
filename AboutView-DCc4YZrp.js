6d1f5e14c21f07c434701cb95c242aa20c4f7da9
CI <CI@example.com>
2024-10-15 08:01:46 +0000

    deploy

---
+++ AboutView-DCc4YZrp.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, a as createBaseVNode, o as openBlock } from "./index-COnfo6G4.js";
+const _sfc_main = {};
+const _hoisted_1 = { class: "about" };
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createElementBlock("div", _hoisted_1, _cache[0] || (_cache[0] = [
+    createBaseVNode("h1", null, "This is an about page", -1)
+  ]));
+}
+const AboutView = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  AboutView as default
+};
---
+++ projectView-asWFguiD.js
@@ -0,0 +1,25 @@
+import { r as ref, b as onMounted, c as createElementBlock, a as createBaseVNode, t as toDisplayString, d as renderSlot, o as openBlock } from "./index-COnfo6G4.js";
+const _hoisted_1 = { class: "about" };
+const _hoisted_2 = { hidden: "true" };
+const _sfc_main = {
+  __name: "projectView",
+  setup(__props) {
+    const aboutContent = ref("");
+    onMounted(() => {
+      console.log(document.getElementsByClassName("about")["0"].attributes.getNamedItem("name").textContent.replace("/project", ""));
+      fetch("/" + document.getElementsByClassName("about")["0"].attributes.getNamedItem("name").textContent.replace("/project", "")).then((response) => response.text()).then((data) => aboutContent.value = data).catch((error) => console.error(error));
+    });
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("div", _hoisted_1, [
+        _cache[0] || (_cache[0] = createBaseVNode("p", { class: "log" }, null, -1)),
+        createBaseVNode("h1", null, toDisplayString(aboutContent.value), 1),
+        createBaseVNode("i", _hoisted_2, [
+          renderSlot(_ctx.$slots, "name")
+        ])
+      ]);
+    };
+  }
+};
+export {
+  _sfc_main as default
+};
