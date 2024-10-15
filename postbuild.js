535b21ce01af4167ec9b872fc06f3be6dcdc0a3a
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-10-15 11:58:18 +0200

    Update postbuild.js

--- postbuild.js
+++ postbuild.js
@@ -3,5 +3,5 @@ const banned=[".git"]
 fs.readdirSync("./dist").map((item)=>{
   console.log(item)
   if(banned.includes(item)){return}
-  fs.renameSync("./dist/"+item,item.split(".")[0].split("-")[0]+"."+item.split(".")[1])
+  fs.renameSync("./dist/"+item,"./dist/"+item.split(".")[0].split("-")[0]+"."+item.split(".")[1])
 })
