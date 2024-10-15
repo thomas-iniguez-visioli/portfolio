f67612f8e814a95ff866d1be3f62e51671ffe125
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-10-15 13:10:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:"/porfolio",
+    base:"/portfolio",
   plugins: [
     vue(),
   ],
   build: {
     minify:false,
-	  base:"/porfolio",
+	  base:"/portfolio",
       rollupOption:{
       output:{
           assetFileNames: "[name].[ext]",
