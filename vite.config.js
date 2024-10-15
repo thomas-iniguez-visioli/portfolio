c20a4640afebb7a6ffa1b7d671eab9c06d39369d
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-10-15 11:40:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,11 @@ export default defineConfig({
                         }
                         
 
-                    }
+                    },
+           chunkFileNames: (chunkInfo) => {
+	 
+          return `${chunkInfo.name.toLowerCase()}.js`;
+        }
       }
       }
     
