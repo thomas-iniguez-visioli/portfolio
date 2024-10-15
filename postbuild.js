78f2083f0e81e894740a7bef379c2142c6741807
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-10-15 12:13:46 +0200

    Update postbuild.js

--- postbuild.js
+++ postbuild.js
@@ -5,6 +5,6 @@ fs.readdirSync("./dist").map((item)=>{
   console.log(item)
   if(banned.includes(item)){return}
     old.map((ech)=>{
-  fs.writeFileSync("./dist/"+item,fs.readFileSync("./dist/"+item).toString().replace(ech,ech.split(".")[0].split("-")[0]+"."+ech.split(".")[1]))}
+  fs.writeFileSync("./dist/"+item,fs.readFileSync("./dist/"+item).toString().replace(ech,ech.split(".")[0].split("-")[0]+"."+ech.split(".")[1])})
   fs.renameSync("./dist/"+item,"./dist/"+item.split(".")[0].split("-")[0]+"."+item.split(".")[1])
 })
