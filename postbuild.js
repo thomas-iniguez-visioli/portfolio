f247ec3c49c8f280e00e095b7cf15db205ac9604
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-10-15 11:55:20 +0200

    Update postbuild.js

--- postbuild.js
+++ postbuild.js
@@ -1,5 +1,7 @@
 import * as fs from "fs"
+const banned=[]
 fs.readdirSync("./dist").map((item)=>{
   console.log(item)
+  if(banned.includes(item)){return}
   fs.renameSync("./dist/"+item,item.split(".")[0].split("-")[0]+"."+item.split(".")[1])
 })
