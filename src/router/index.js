f9f7d70c3438d3c042b015a46e276327c3d0d49b
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-10-16 16:44:20 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const gen=(p)=>{
 return ""+p.params.name+".txt"
 }
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
