4ac5734d5248484a94c4d8f03d6e9094ba4d75ea
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2024-10-17 17:04:34 +0200

    cvp2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,10 +7,10 @@
   </div>
 </template>
 <script setup>
-
+/*
 fetch("/portfolio/cv.txt").then(response => response.text())
     .then(data => document.getElementsByClassName("about")["0"].innerHTML = data.replace("thomas.iniguez@free.fr","thomas.iniguez+<le nom de votre entreprise>@free.fr"))
-    .catch(error => console.error(error))
+    .catch(error => console.error(error))*/
 </script>
 <style>
 @media (min-width: 1024px) {
