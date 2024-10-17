8c9e4af2e48681c05dca165e3f4b289c4e26574d
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2024-10-17 16:04:08 +0200

    cvp1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,21 @@
 <template>
+
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import {  onMounted } from 'vue'
+onMounted(() => {
+  var url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf';
 
+  // Loaded via <script> tag, create shortcut to access PDF.js exports.
+  var { pdfjsLib } = globalThis;
+
+  // The workerSrc property shall be specified.
+  pdfjsLib.GlobalWorkerOptions.workerSrc = `/pdf.worker.mjs`;
+})
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
