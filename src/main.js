bd121e52d1667489c815414d767638c3f1fed70f
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2024-11-05 10:55:09 +0100

    t3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import './main.css'
 
-import { createApp } from 'vue'
+import { createApp,Vue } from 'vue'
 import { createPinia } from 'pinia'
-
+import VueLogger from 'vuejs-logger';
+const isProduction = process.env.NODE_ENV === 'production';
+ 
+const options = {
+    isEnabled: true,
+    logLevel : isProduction ? 'error' : 'debug',
+    stringifyArguments : false,
+    showLogLevel : true,
+    showMethodName : true,
+    separator: '|',
+    showConsoleColors: true
+};
+ 
+Vue.use(VueLogger, options);
 import App from './App.vue'
 import router from './router'
 
