20b7810bc041c65e5dc8cdb523d9f47549c22091
CI <CI@example.com>
2024-11-05 09:42:02 +0000

    build

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -2,14 +2,11 @@
 <script setup>
 import WelcomeItem from './WelcomeItem.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+
 </script>
 
 <template>
-  <p></p><p></p>
+<p></p><p></p>
   <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
@@ -17,42 +14,15 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #heading>RGPD</template>
 
    
-  </WelcomeItem>
-  <p></p><p></p>
+  </WelcomeItem>,<p></p><p></p>
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
+      <DocumentationIcon />
     </template>
     <template #heading>portfolio</template>
 
    
-
-
   </WelcomeItem>
-  <p></p><p></p><p></p><p></p>
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-   
-  </WelcomeItem>
-  <p></p><p></p>
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-
-  </WelcomeItem>
-  <p></p><p></p>
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-  </WelcomeItem>
+  
+  
 </template>
