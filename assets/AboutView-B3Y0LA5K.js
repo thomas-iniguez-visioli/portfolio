6c1c0c5530a23592b14405e221744d678cc95e24
CI <CI@example.com>
2024-11-05 10:12:32 +0000

    deploy

--- assets/AboutView-B3Y0LA5K.js
+++ assets/AboutView-B3Y0LA5K.js
@@ -1,4 +1,4 @@
-import { c as createElementBlock, b as createBaseVNode, a as openBlock } from "./index-vqlziQnt.js";
+import { c as createElementBlock, b as createBaseVNode, a as openBlock } from "./index-DRhdC02R.js";
 const _imports_0 = "/portfolio/assets/cv-Bbg8av0A.png";
 const _hoisted_1 = { class: "about" };
 const _sfc_main = {
@@ -6,7 +6,6 @@ const _sfc_main = {
   setup(__props) {
     return (_ctx, _cache) => {
       return openBlock(), createElementBlock("div", _hoisted_1, _cache[0] || (_cache[0] = [
-        createBaseVNode("h1", null, "This is an about page", -1),
         createBaseVNode("img", {
           src: _imports_0,
           alt: "mon cv "
