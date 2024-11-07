2f54269129359e86dbc3888c749b70b7f371f8a5
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2024-11-07 16:59:19 +0100

    update

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,7 @@ import { createLogger, StringifyObjectsHook, LoggerHook, LogEvent } from 'vue-lo
 const ServerLogHook = {
     run(event) {
         console.log(event.argumentArray[0])
-      console.log(event.level+"     "+  event.data )
+      console.log(event.level+"     "+  event.argumentArray )
     }
     // example using async/await:
     // async run(event: LogEvent) {
