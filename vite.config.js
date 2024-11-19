ab74b4b36ff3352f026cf05b746188106ba98a09
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2024-11-19 17:06:44 +0100

    temp

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
   ],configureWebpack:{
     mode: 'development',
-    devtool: false,
+    devtool: true,
     optimization: {
       splitChunks: {
         
