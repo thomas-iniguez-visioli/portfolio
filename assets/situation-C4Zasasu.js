e413539f835cfade64f90e44f9e83b342a38a3a1
CI <CI@example.com>
2024-11-20 16:14:54 +0000

    deploy

--- assets/situation-C4Zasasu.js
+++ assets/situation-C4Zasasu.js
@@ -1,11 +1,11 @@
-import { _ as _export_sfc, t as temp, o as onMounted, a as openBlock, c as createElementBlock, r as renderSlot, b as createBaseVNode, d as createVNode, w as withCtx, e as createTextVNode, F as Fragment } from "./index-_NSwmbD7.js";
-import { D as DocumentationIcon } from "./IconDocumentation-D9vNp7s7.js";
+import { _ as _export_sfc, t as temp, o as onMounted, a as openBlock, c as createElementBlock, r as renderSlot, b as createBaseVNode, d as createVNode, w as withCtx, F as Fragment, e as createTextVNode } from "./index-FOKD68nC.js";
+import { D as DocumentationIcon } from "./IconDocumentation-3pjzYr0U.js";
 const _hoisted_1 = {
   href: "/",
   id: "redirect"
 };
 const _sfc_main$2 = {
-  __name: "projetItem",
+  __name: "situationItem",
   setup(__props) {
     const log = temp.setup();
     onMounted(() => {
@@ -13,7 +13,7 @@ const _sfc_main$2 = {
         const item = document.getElementsByTagName("a")[ite];
         console.log(item.href);
         if (item.id) {
-          log.info(item.href.includes("local"));
+          log.info(item.href.includes("situation"));
           item.href = window.location.href + item.textContent.split("/")[item.textContent.split("/").length - 1];
           log.info(item.href);
         }
@@ -26,32 +26,20 @@ const _sfc_main$2 = {
     };
   }
 };
-const WelcomeItem = /* @__PURE__ */ _export_sfc(_sfc_main$2, [["__scopeId", "data-v-a6a9fdf5"]]);
+const WelcomeItem = /* @__PURE__ */ _export_sfc(_sfc_main$2, [["__scopeId", "data-v-c6479ac7"]]);
 const _sfc_main$1 = {
-  __name: "projet",
+  __name: "situation",
   setup(__props) {
     return (_ctx, _cache) => {
       return openBlock(), createElementBlock(Fragment, null, [
+        _cache[1] || (_cache[1] = createBaseVNode("p", null, null, -1)),
         _cache[2] || (_cache[2] = createBaseVNode("p", null, null, -1)),
-        _cache[3] || (_cache[3] = createBaseVNode("p", null, null, -1)),
         createVNode(WelcomeItem, null, {
           icon: withCtx(() => [
             createVNode(DocumentationIcon)
           ]),
           heading: withCtx(() => _cache[0] || (_cache[0] = [
-            createTextVNode("RGPD")
-          ])),
-          _: 1
-        }),
-        _cache[4] || (_cache[4] = createTextVNode(",")),
-        _cache[5] || (_cache[5] = createBaseVNode("p", null, null, -1)),
-        _cache[6] || (_cache[6] = createBaseVNode("p", null, null, -1)),
-        createVNode(WelcomeItem, null, {
-          icon: withCtx(() => [
-            createVNode(DocumentationIcon)
-          ]),
-          heading: withCtx(() => _cache[1] || (_cache[1] = [
-            createTextVNode("france-nuit")
+            createTextVNode("1")
           ])),
           _: 1
         })
@@ -60,11 +48,11 @@ const _sfc_main$1 = {
   }
 };
 const _sfc_main = {
-  __name: "HomeView",
+  __name: "situation",
   setup(__props) {
     return (_ctx, _cache) => {
       return openBlock(), createElementBlock("main", null, [
-        _cache[0] || (_cache[0] = createTextVNode(" bien le bonjour voici mes différent projet ")),
+        _cache[0] || (_cache[0] = createTextVNode(" bien le bonjour voici les différente situation professionelle dans lequel je me suis trouvée ")),
         createVNode(_sfc_main$1)
       ]);
     };
