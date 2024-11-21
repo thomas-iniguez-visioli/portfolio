958fa323faf3e2fe43a555b7b061391dda0d303a
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2024-11-21 13:50:26 +0100

    patch

--- postbuild.js
+++ postbuild.js
@@ -45,7 +45,7 @@ Object.keys(document.getElementsByTagName("a")).map((ite)=>{
     //console.log(item.parentElement)
     log.info(item.href.includes("${file.name}"))
 
-      item.href=window.location.href+"/${file.name}"+item.textContent.split("/")[item.textContent.split("/").length-1]
+      item.href=window.location.href+"${file.name}/"+item.textContent.split("/")[item.textContent.split("/").length-1]
     
     
     log.info(item.href)
