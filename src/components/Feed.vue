163a170cfb4a8f338d044f4d749c839a67193129
arbinger <thomas.iniguez+github@free.fr>
2024-11-23 14:33:56 +0100

    add veille

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -59,7 +59,7 @@ export default {
       this.loading = true;
       this.feed = {};
       try {
-        const data = await fetch(this.feedUrl);
+        const data = await fetch("https:"+"/"+"/"+this.feedUrl);
         if (data.ok) {
           const text = await data.text();
           const parser = new RSSParser();
