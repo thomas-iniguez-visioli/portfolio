cecdc7716ad3a4785689af467c2f3534a8e2d27b
arbinger <thomas.iniguez+github@free.fr>
2024-11-24 11:16:25 +0100

    update

--- postbuild.js
+++ postbuild.js
@@ -2,6 +2,11 @@ import * as fs from'fs'
 fs.readdirSync("./dist").map((file)=>{
     if(file=='.git'){
         return
+    }if(file=='projet'){
+        return
+    }
+    if(file=='situation'){
+        return
     }
 
     fs.writeFileSync("./dist/"+file,fs.readFileSync("./dist/"+file).toString().replace("]]>"," >"))
