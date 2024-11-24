fa573942c3c86ad08fde59d5d607ba000ff858a9
arbinger <thomas.iniguez+github@free.fr>
2024-11-24 07:49:57 +0100

    test

--- src/views/suiviView.vue
+++ src/views/suiviView.vue
@@ -26,7 +26,7 @@ async function fetchRSSFeed(url) {
       console.log(title)
       const link = url;
       console.log(link)
-      const description = cod.documentElement.querySelector('description').textContent.replace("<li>","<br>").replace("</li>","<br>").replace("]]>"," ");
+      const description = cod.documentElement.querySelector('description').innerHTML;
       console.log(description)
       return `<a href="${link}" target="_blank">fuite numéro ${ar.length-id}:  ${title}</a><br>${description}<br>`;
     }).join('<br>');
