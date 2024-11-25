f1b7f3aefa3e16f6d118b5d9df65335f8fc1c254
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2024-11-25 11:22:45 +0100

    t2

--- src/views/suiviView.vue
+++ src/views/suiviView.vue
@@ -32,7 +32,7 @@ async function fetchRSSFeed(url) {
       console.log(link)
       const description = cod.documentElement.querySelector('description').innerHTML.replace("<!--[CDATA["," ").replace("]]>"," ");;
       console.log(description)
-      return `<a href="${link}" target="_blank">fuite numéro ${ar.length-id}:  ${title}</a><br>${description.replace("]]>"," ")}<br>`;
+      return `<a href="${link}" target="_blank">fuite numéro ${ar.length-id}:  ${title}</a><br>${description.replace("]]&gt"," ")}<br>`;
     }).join('<br>');
     document.querySelector('.rss').innerHTML = rssContent;
     } catch (error) {
