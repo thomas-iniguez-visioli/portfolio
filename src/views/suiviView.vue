1ba08fb45688546b8b79b7a992eb773cf51b72cc
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-11-26 15:43:51 +0100

    Update suiviView.vue

--- src/views/suiviView.vue
+++ src/views/suiviView.vue
@@ -30,9 +30,9 @@ async function fetchRSSFeed(url) {
       console.log(title)
       const link = "https://bonjourlafuite.eu.org";
       console.log(link)
-      const description = cod.documentElement.querySelector('description').innerHTML.replace("<!--[CDATA[","").replace(";","").replace("]]","").replace("-->"," ");;
+      const description = cod.documentElement.querySelector('description').innerHTML.replace("<!--[CDATA[","").replace(";","").replace("]]","");;
       console.log(description)
-      return `<a href="${link}" target="_blank">fuite numéro ${ar.length-id}:  ${title}</a><br>${description.replace("]]&gt","</ul>")}<br>`;
+      return `<a href="${link}" target="_blank">fuite numéro ${ar.length-id}:  ${title}</a><br>${description.replace("]]&gt","</ul>").replace("-->"," ")}<br>`;
     }).join('<br>');
     document.querySelector('.rss').innerHTML = rssContent;
     } catch (error) {
