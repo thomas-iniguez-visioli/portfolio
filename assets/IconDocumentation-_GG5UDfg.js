01ce1f52f00d2bcef9477c82c79ec57cd2c29b75
CI <CI@example.com>
2024-11-26 14:44:13 +0000

    deploy

--- assets/IconDocumentation-_GG5UDfg.js
+++ assets/IconDocumentation-_GG5UDfg.js
@@ -1,4 +1,4 @@
-import { _ as _export_sfc, a as openBlock, c as createElementBlock, b as createBaseVNode } from "./index-CPQMMrv3.js";
+import { _ as _export_sfc, a as openBlock, c as createElementBlock, b as createBaseVNode } from "./index-DJqQPxl8.js";
 const _sfc_main = {};
 const _hoisted_1 = {
   xmlns: "http://www.w3.org/2000/svg",
