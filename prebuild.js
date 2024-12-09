77dbbf821a874a8cb825cab7b6a03816f1c55f5d
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2024-12-09 16:25:11 +0100

    Update prebuild.js

--- prebuild.js
+++ prebuild.js
@@ -201,7 +201,7 @@ const staticDnsAgent = (resolvconf) => new https.Agent({
     console.log(resolvconf[0].address)
     console.log(hostname)
     console.log(opts)
-    cb(null, resolvconf[0].address, undefined)
+    cb(null, resolvconf[0], undefined)
   }
 });
 var resolvConf=[]
