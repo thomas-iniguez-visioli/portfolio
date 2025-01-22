d503604cea31de3801ad60d04f3d5c33badfdf61
arbinger <thomas.iniguez+github@free.fr>
2025-01-22 20:16:32 +0100

    p2

--- postbuild.js
+++ postbuild.js
@@ -11,3 +11,14 @@ fs.readdirSync("./dist/assets").map((file)=>{
 
     fs.writeFileSync("./dist/assets/"+file,fs.readFileSync("./dist/assets/"+file).toString().replace("]]>"," >"))
 })
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://thomas-iniguez-visioli.github.io/portfolio/test.html'); // Remplacez par l'URL que vous souhaitez scraper
+  await page.pdf({ path: 'public/cv_stage_bts.pdf', format: 'A4' }); 
+  await page.pdf({ path: 'dist/cv_stage_bts.pdf', format: 'A4' }); 
+  await browser.close();
+})();
+
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,51 @@
   <div ref="cv"></div>
 </template>
 <script setup>
+var url = 'cv_stage_bts.pdf'
 
-import { ref } from 'vue';
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var { pdfjsLib } = globalThis
+
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.mjs'
+
+// Asynchronous download of PDF
+var loadingTask = pdfjsLib.getDocument(url)
+loadingTask.promise.then(
+  function (pdf) {
+    console.log('PDF loaded')
+
+    // Fetch the first page
+    var pageNumber = 1
+    pdf.getPage(pageNumber).then(function (page) {
+      console.log('Page loaded')
+
+      var scale = 1
+      var viewport = page.getViewport({ scale: scale })
+
+      // Prepare canvas using PDF page dimensions
+      var canvas = document.getElementById('the-canvas')
+      var context = canvas.getContext('2d')
+      canvas.height = viewport.height
+      canvas.width = viewport.width
+      console.log(canvas)
+      // Render PDF page into canvas context
+      var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+      }
+      var renderTask = page.render(renderContext)
+      renderTask.promise.then(function () {
+        console.log('Page rendered')
+      })
+    })
+  },
+  function (reason) {
+    // PDF loading error
+    console.error(reason)
+  }
+)
+/*import { ref } from 'vue';
 
 const cvContent = ref('cv');
 
