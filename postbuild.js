1aaf88ccd9c88f77cd723d2077daa4b39a8500e6
arbinger <thomas.iniguez+github@free.fr>
2025-01-25 22:04:12 +0100

    't'

--- postbuild.js
+++ postbuild.js
@@ -17,8 +17,8 @@ import puppeteer from 'puppeteer';
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://thomas-iniguez-visioli.github.io/portfolio/test.html', { waitUntil: 'load' }); // Remplacez par l'URL que vous souhaitez scraper
-  //await page.pdf({ path: 'public/cv_stage_bts.pdf', format: 'A4' }); 
-  await page.pdf({ path: 'dist/cv_stage_bts.pdf'}); 
+  await page.pdf({ path: 'public/cv_stage_bts.pdf',  margin: { top: '1px', right: '1px', bottom: '1px', left: '1px' } }); 
+  await page.pdf({ path: 'dist/cv_stage_bts.pdf',  margin: { top: '1px', right: '1px', bottom: '1px', left: '1px' } }); 
   await browser.close();
 })();
 
