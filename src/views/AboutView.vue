647ed8996c009edf390598abeda6b70990c90d00
CI <CI@example.com>
2025-01-25 22:42:38 +0000

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,14 +5,14 @@
 const downloadCV = () => {
   var url = 'cv_stage_bts.pdf'
   fetch(url)
-    .then(response => response.blob())
-    .then(blob => {
-      var link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      link.download = 'cv_stage_bts.pdf';
-      link.click();
+    .then((response) => response.blob())
+    .then((blob) => {
+      var link = document.createElement('a')
+      link.href = URL.createObjectURL(blob)
+      link.download = 'cv_stage_bts.pdf'
+      link.click()
     })
-    .catch(error => console.error('Erreur de téléchargement du PDF:', error));
+    .catch((error) => console.error('Erreur de téléchargement du PDF:', error))
 }
 </script>
 <style>
