5916e00ef0062a5b4de38d0e0b3eef5766c67e35
arbinger <thomas.iniguez+github@free.fr>
2025-01-25 23:05:02 +0100

    Update PDF generation script to handle existing PDF files and fix image path in skills section

--- postbuild.js
+++ postbuild.js
@@ -17,7 +17,13 @@ import puppeteer from 'puppeteer';
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://thomas-iniguez-visioli.github.io/portfolio/test.html', { waitUntil: 'load' }); // Remplacez par l'URL que vous souhaitez scraper
+  if (fs.existsSync('public/cv_stage_bts.pdf')) {
+    fs.unlinkSync('public/cv_stage_bts.pdf');
+  }
   await page.pdf({ path: 'public/cv_stage_bts.pdf',  margin: { top: '1px', right: '1px', bottom: '1px', left: '1px' } }); 
+  if (fs.existsSync('dist/cv_stage_bts.pdf')) {
+    fs.unlinkSync('dist/cv_stage_bts.pdf');
+  }
   await page.pdf({ path: 'dist/cv_stage_bts.pdf',  margin: { top: '1px', right: '1px', bottom: '1px', left: '1px' } }); 
   await browser.close();
 })();
