88232a61e23ad2f25c2787a8023d500fa8665334
ci <temp@free.fr>
2025-02-27 15:13:09 +0000

    doc: generate weekly retro

--- docs/index.js
+++ docs/index.js
@@ -6678,7 +6678,7 @@
     };
   
     var pdfUnescape = function pdfUnescape(value) {
-      return value.replace(/\\\(/g, "(").replace(/\\\)/g, ")").replace(/\\\\/g, "\\");
+      return value.replace(/\\\\/g, "\\").replace(/\\\(/g, "(").replace(/\\\)/g, ")");
     };
   
     var f2 = function f2(number) {
