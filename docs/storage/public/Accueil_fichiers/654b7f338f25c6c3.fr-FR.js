ade88d44cf422a3fa2797dd191745c099569adf4
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-02-27 14:49:26 +0100

    Delete docs directory

--- docs/storage/public/Accueil_fichiers/654b7f338f25c6c3.fr-FR.js
+++
@@ -1,5 +0,0 @@
-window['cmsg']['assets'] = window['cmsg']['assets'] || {}
-window['cmsg']['assets']['fr-FR'] = Object.assign(window['cmsg']['assets']['fr-FR'] || {}, {
-  5706: { js: ['abc0c52ae63f9bdc.strings.js'], css: [] },
-  8701: { js: ['f9f90d671b5d5495.strings.js'], css: [] }
-})
