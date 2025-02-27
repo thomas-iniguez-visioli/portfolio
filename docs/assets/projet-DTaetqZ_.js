27a44fe2d451cb2e712b512ceb6dea62b11c8a7c
ci <temp@free.fr>
2025-02-27 14:11:43 +0000

    doc: generate weekly retro

---
+++ docs/assets/projet-DTaetqZ_.js
@@ -0,0 +1,89 @@
+import { _ as _export_sfc, t as temp, d as onMounted, c as createElementBlock, r as renderSlot, o as openBlock, e as createBaseVNode, b as createVNode, a as createTextVNode, w as withCtx, F as Fragment } from "./index-CHIhqd4T.js";
+import { D as DocumentationIcon } from "./IconDocumentation-BvHQ_zGX.js";
+const _hoisted_1 = {
+  href: "/",
+  id: "redirect"
+};
+const _sfc_main$1 = {
+  __name: "projetItem",
+  setup(__props) {
+    const log = temp.setup();
+    onMounted(() => {
+      Object.keys(document.getElementsByTagName("a")).map((ite) => {
+        const item = document.getElementsByTagName("a")[ite];
+        console.log(item.href);
+        if (item.id) {
+          log.info(item.href.includes("projet"));
+          item.href = window.location.href + "projet/" + item.textContent.split("/")[item.textContent.split("/").length - 1];
+          item.href = item.href.replace("situationsituation", "situation").replace("projetprojet", "projet");
+          log.info(item.href);
+        }
+      });
+    });
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("a", _hoisted_1, [
+        renderSlot(_ctx.$slots, "heading", { id: "name" }, void 0, true)
+      ]);
+    };
+  }
+};
+const WelcomeItem = /* @__PURE__ */ _export_sfc(_sfc_main$1, [["__scopeId", "data-v-6b13e6e2"]]);
+const _sfc_main = {
+  __name: "projet",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock(Fragment, null, [
+        _cache[4] || (_cache[4] = createBaseVNode("p", null, null, -1)),
+        _cache[5] || (_cache[5] = createBaseVNode("p", null, null, -1)),
+        createVNode(WelcomeItem, null, {
+          icon: withCtx(() => [
+            createVNode(DocumentationIcon)
+          ]),
+          heading: withCtx(() => _cache[0] || (_cache[0] = [
+            createTextVNode("RGPD")
+          ])),
+          _: 1
+        }),
+        _cache[6] || (_cache[6] = createTextVNode(",")),
+        _cache[7] || (_cache[7] = createBaseVNode("p", null, null, -1)),
+        _cache[8] || (_cache[8] = createBaseVNode("p", null, null, -1)),
+        createVNode(WelcomeItem, null, {
+          icon: withCtx(() => [
+            createVNode(DocumentationIcon)
+          ]),
+          heading: withCtx(() => _cache[1] || (_cache[1] = [
+            createTextVNode("francenuit")
+          ])),
+          _: 1
+        }),
+        _cache[9] || (_cache[9] = createTextVNode(",")),
+        _cache[10] || (_cache[10] = createBaseVNode("p", null, null, -1)),
+        _cache[11] || (_cache[11] = createBaseVNode("p", null, null, -1)),
+        createVNode(WelcomeItem, null, {
+          icon: withCtx(() => [
+            createVNode(DocumentationIcon)
+          ]),
+          heading: withCtx(() => _cache[2] || (_cache[2] = [
+            createTextVNode("uptime")
+          ])),
+          _: 1
+        }),
+        _cache[12] || (_cache[12] = createTextVNode(",")),
+        _cache[13] || (_cache[13] = createBaseVNode("p", null, null, -1)),
+        _cache[14] || (_cache[14] = createBaseVNode("p", null, null, -1)),
+        createVNode(WelcomeItem, null, {
+          icon: withCtx(() => [
+            createVNode(DocumentationIcon)
+          ]),
+          heading: withCtx(() => _cache[3] || (_cache[3] = [
+            createTextVNode("youtube")
+          ])),
+          _: 1
+        })
+      ], 64);
+    };
+  }
+};
+export {
+  _sfc_main as default
+};
---
+++ docs/assets/suiviView-MML6x7oc.js
@@ -0,0 +1,65 @@
+import { d as onMounted, c as createElementBlock, e as createBaseVNode, o as openBlock } from "./index-CHIhqd4T.js";
+const _sfc_main = {
+  __name: "suiviView",
+  setup(__props) {
+    onMounted(() => {
+      async function fetchRSSFeed(url) {
+        try {
+          const response = await fetch(url, { mode: "no-cors" });
+          if (!response.ok) {
+            console.log(response);
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          const rssText = await response.text();
+          const parser = new DOMParser();
+          const xmlDoc = parser.parseFromString(rssText, "text/xml");
+          const items = xmlDoc.querySelectorAll("entry");
+          const d = ["p", "div"];
+          const rssContent = Array.from(items).map((item) => {
+            const cod = new DOMParser().parseFromString(item.outerHTML, "text/xml");
+            var description = cod.documentElement.querySelector("content").innerHTML.replace("]]>", "");
+            console.log(
+              new DOMParser().parseFromString(description, "text/html").documentElement.querySelector("a").href.split("#")[1].split("-").slice(-3).join("-")
+            );
+            if (cod.documentElement.querySelector("content").innerHTML == "<!--[CDATA[]]-->") {
+              description = "<ul><li>inconnu</li></ul>";
+            }
+            description = description.replace(`<!--[CDATA[<p-->`, `<p> `);
+            return {
+              content: `<hr/><strong>${new DOMParser().parseFromString(description, "text/html").documentElement.querySelector("a").href.split("#")[1].split("-").slice(-3).join("-")}<br/><strong>
+              ${decodeURI(
+                new DOMParser().parseFromString(description, "text/html").documentElement.querySelector("a").href.split("#")[1].split("-")[0]
+              )}</strong><br/>${description}`,
+              date: new DOMParser().parseFromString(description, "text/html").documentElement.querySelector("a").href.split("#")[1].split("-").slice(-3).join("-")
+            };
+          }).map((item) => {
+            var data = item.date.split("-");
+            var date = `${data[0]}-${data[1]}-${data[2]}`;
+            item.date = date;
+            return item;
+          }).sort((a, b) => {
+            return b.date - a.date;
+          }).map((item) => {
+            console.log(item.content);
+            return item.content;
+          }).join("<hr/><br/>");
+          document.querySelector(".rss").innerHTML = rssContent;
+        } catch (error) {
+          console.error("Error type 2 fetching RSS feed:", error);
+        }
+      }
+      const rssUrl = "/portfolio/feed.xml";
+      fetchRSSFeed(rssUrl);
+    });
+    return (_ctx, _cache) => {
+      return openBlock(), createElementBlock("main", null, _cache[0] || (_cache[0] = [
+        createBaseVNode("div", { class: "rss" }, [
+          createBaseVNode("h6", null, "chargement en cours ...")
+        ], -1)
+      ]));
+    };
+  }
+};
+export {
+  _sfc_main as default
+};
