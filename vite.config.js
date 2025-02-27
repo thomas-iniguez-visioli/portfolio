191b0ca40869b16f5f5b13f37d03dfc3e79cda90
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-02-27 14:51:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     base:"/portfolio",
+	outputDir:"./docs",
   plugins: [
     vue({
              include: [/\.vue$/, /\.md$/] // ← Treat MD files as Vue components
