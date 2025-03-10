faad1a4f378ccdb30ff763f5b8fe86abf3eedb5f
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2025-03-10 16:26:07 +0100

    Update somebody.js

--- src/themes/hexo-theme/scripts/somebody.js
+++ src/themes/hexo-theme/scripts/somebody.js
@@ -1,5 +1,6 @@
 'use strict';
-
-const somebody = import('somebody');
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const somebody = require('somebody');
 
 hexo.extend.helper.register('somebody', somebody.parse.bind(somebody));
